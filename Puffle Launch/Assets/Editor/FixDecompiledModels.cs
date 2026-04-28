88335b6b16234a24752a36a63c96286cdd6f7e7c
Eleven <21669120+wednesday2024@users.noreply.github.com>
2026-04-28 15:55:11 -0500

    Upload.

---
+++ Puffle Launch/Assets/Editor/FixDecompiledModels.cs
@@ -0,0 +1,114 @@
+using UnityEngine;
+using UnityEditor;
+
+public class FixDecompiledModels
+{
+    private const string TARGET_PATH = "Assets/";
+
+    private static readonly string[] SKIP_FOLDERS = new string[]
+    {
+        "Modules",
+        "Town",
+        "Boardwalk",
+        "Diving",
+        "BoxDimension",
+        "MtBlizzard",
+        "Beach",
+        "HerbertBase",
+        "Custom",
+        "DisneyCDN",
+        "MtBlizzardSummit"
+    };
+
+    [MenuItem("Project/Models/Fix Decompiled Models")]
+    public static void FixMeshes()
+    {
+        string[] guids = AssetDatabase.FindAssets("t:Mesh", new[] { TARGET_PATH });
+
+        for (int i = 0; i < guids.Length; i++)
+        {
+            string path = AssetDatabase.GUIDToAssetPath(guids[i]);
+
+            if (EditorUtility.DisplayCancelableProgressBar("Fixing Models", path, (float)i / guids.Length))
+                break;
+
+            Mesh mesh = AssetDatabase.LoadAssetAtPath<Mesh>(path);
+            if (mesh == null)
+                continue;
+
+            if (ShouldSkip(path))
+                continue;
+
+            FixMesh(mesh);
+        }
+
+        EditorUtility.ClearProgressBar();
+        AssetDatabase.SaveAssets();
+        AssetDatabase.Refresh();
+    }
+
+    private static bool ShouldSkip(string path)
+    {
+        for (int i = 0; i < SKIP_FOLDERS.Length; i++)
+        {
+            if (path.Contains("/" + SKIP_FOLDERS[i] + "/"))
+                return true;
+        }
+        return false;
+    }
+
+    private static void FixMesh(Mesh mesh)
+    {
+        int vCount = mesh.vertexCount;
+        if (vCount == 0)
+            return;
+
+        Vector3[] vertices = mesh.vertices;
+
+        Vector2[] uv0 = mesh.uv;
+        if (uv0 == null || uv0.Length != vCount)
+            uv0 = new Vector2[vCount];
+
+        Vector2[] uv1 = mesh.uv2;
+        if (uv1 == null || uv1.Length != vCount)
+            uv1 = new Vector2[vCount];
+
+        Color32[] colors = mesh.colors32;
+        if (colors == null || colors.Length != vCount || IsAllZero(colors))
+        {
+            colors = new Color32[vCount];
+            for (int i = 0; i < vCount; i++)
+                colors[i] = new Color32(255, 255, 255, 255);
+        }
+
+        Vector3[] normals = mesh.normals;
+        if (normals == null || normals.Length != vCount)
+        {
+            mesh.RecalculateNormals();
+            normals = mesh.normals;
+        }
+
+        Vector4[] tangents = mesh.tangents;
+        if ((tangents == null || tangents.Length != vCount) && uv0 != null && normals != null)
+            mesh.RecalculateTangents();
+
+        mesh.RecalculateBounds();
+
+        mesh.uv = uv0;
+        mesh.uv2 = uv1;
+        mesh.colors32 = colors;
+
+        EditorUtility.SetDirty(mesh);
+        AssetDatabase.SaveAssetIfDirty(mesh);
+    }
+
+    private static bool IsAllZero(Color32[] colors)
+    {
+        for (int i = 0; i < colors.Length; i++)
+        {
+            if (colors[i].r != 0 || colors[i].g != 0 || colors[i].b != 0 || colors[i].a != 0)
+                return false;
+        }
+        return true;
+    }
+}
